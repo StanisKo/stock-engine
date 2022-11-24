e16785f603eb4131ae8d2bd6c64b6a06b53b290b
Stan Konnov <stanislav@jointheview.com>
2022-11-24 12:36:33 +0100

    working on profie docs

--- src/schemas/stock-profile.schema.ts
+++ src/schemas/stock-profile.schema.ts
@@ -2,12 +2,27 @@ import * as uuid from 'uuid';
 
 import mongoose from 'mongoose';
 
+/*
+NOTE: most of the measurement and ratios fields are NOT required, since companies do
+not expose everything in their financial statements and, therefeore, values
+are not available via the API
+
+Iteration 1 (24-11-2022):
+
+This profile is the result of the average between 2 of my core positions: NIO and APPS
+
+This might change based on further business ideas
+*/
+
 const { Schema } = mongoose;
 
 const stockProfileSchema = new Schema(
     {
         _id: { type: mongoose.SchemaTypes.String, default: uuid.v4 },
 
+        /*
+        Size measurement of the stock
+        */
         marketCap: {
 
             label: {
@@ -24,16 +39,38 @@ const stockProfileSchema = new Schema(
 
         risk: {
 
+            /*
+            Indicates how much the current return is deviating from its expected historical normal returns.
+            The higher standard deviation, the greater possible outcomes, both negative and positive.
+            */
             standardDeviation: {
                 type: Number,
                 required: true
             },
 
+            /*
+            Measures rate of return on the asset above risk-free investment, such as treasury bonds or cash.
+            In other words, measures whether the risk is justified against investing into risk-free assets.
+            A Sharpe Ratio above 1.0 is considered good, as it indicates potential excess return
+            relative to the volatility of the asset. 
+            */
             sharpeRatio: {
                 type: Number,
                 required: true
             },
 
+            /*
+            Measures volatility of an asset against the broader index (S&P 500, Dow Jones, Nasdaq, etc.).
+            Displays the intensity of movement, not the direction: the asset moves in the same direction as index,
+            either slower, same, or faster.
+
+            Beta Ranges:
+
+                * > 0 && < 1.0: Less volatile than the index.
+                * == 1.0: Mirrors the volatility of the index.
+                * > 1.0: More volatile than the index.
+                * < 0: Moves opposite direction of the index (very rare).
+            */
             beta: {
                 type: Number,
                 required: true
