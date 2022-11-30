e66bcdd1733834e5974105b44b9428fa805891ca
Stan Konnov <stanislav@jointheview.com>
2022-11-30 19:31:16 +0100

    setup for cagr calculation

--- src/services/calculators/cagr-calculator.service.ts
+++ src/services/calculators/cagr-calculator.service.ts
@@ -22,6 +22,28 @@ beginningValue = first price (* 1, duh)
 # of years = sum of unique years in prices dataset
 */
 
+import { ITickerPrice, ITickerSplit } from '../../interfaces/ticker.interface';
+
 export class CAGRCalculatorService {
+
+    startingPrice: number;
+
+    endingPrice: number;
+
+    numberOfUniqueYears: number;
+
+    /*
+    We can immediately declare it as a constant
+    */
+    base: 1;
+
+    constructor(startingPrice: number, endindPrice: number, prices: ITickerPrice[], splits: ITickerSplit[]) {
+
+        this.startingPrice = startingPrice;
+
+        this.endingPrice = endindPrice;
+
+
+    }
 
 }
