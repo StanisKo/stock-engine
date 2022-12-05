24a3952582f4bc245c7d9057db5fa39a13664acb
Stan Konnov <stanislav@jointheview.com>
2022-12-05 14:37:58 +0100

    a tad of refactor

--- src/services/api/financial-api-parser.service.ts
+++ src/services/api/financial-api-parser.service.ts
@@ -28,8 +28,6 @@ export class FinancialApiParserService {
 
     rawTickerData: ITickerFinancialData;
 
-    standardDeviationCalculatorService: StandardDeviationCalculatorService;
-
     constructor(rawTickerData: ITickerFinancialData) {
 
         this.extractedTickerData = {} as IIndustryProfile;
