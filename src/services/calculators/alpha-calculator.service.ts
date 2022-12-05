81d3b0c2b30e302f4fbd367437a78666bbdbdda8
Stan Konnov <stanislav@jointheview.com>
2022-12-05 21:13:53 +0100

    todos

--- src/services/calculators/alpha-calculator.service.ts
+++ src/services/calculators/alpha-calculator.service.ts
@@ -9,9 +9,9 @@ Beta represents the systematic risk of a portfolio
 
 Rm represents the benchmark return
 
-Simply, this can also be CAGR of the asset - CAGR of the index?
-
 On Alpha: https://www.investopedia.com/terms/a/alpha.asp
+
+TODO: implement above, don't use CAGR, use SP500 RoR and US 10Y treasury yield
 */
 
 export class AlphaCalculatorService {
--- src/services/calculators/sharpe-ratio-calculator.service.ts
+++ src/services/calculators/sharpe-ratio-calculator.service.ts
@@ -7,6 +7,8 @@ Sharpe Ratio has to be calculated over period of time,
 in our case -- TTM, Trailing Twelve Month RoR of ticker and benchmark
 
 On Sharpe Ratio: https://www.investopedia.com/terms/s/sharperatio.asp
+
+TODO: base it on treasury yield
 */
 
 export class SharpeRatioCalculatorService {
