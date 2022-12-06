2678b3de18fe7051662a534abedd9cd063fca794
Stan Konnov <stanislav@jointheview.com>
2022-12-06 13:01:02 +0100

    r squared setup

---
+++ src/services/calculators/r-squared-calculator.service.ts
@@ -0,0 +1,11 @@
+/*
+R-Squared — measures how much the movement of the assets is influenced by the movement of the index.
+Ranges from 0 to 100.
+
+On R-Squared: https://www.investopedia.com/terms/r/r-squared.asp
+*/
+
+export class RSquaredCalculatorService {
+
+
+}
