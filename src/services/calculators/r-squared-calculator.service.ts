c5792137b97e1364191302381f68ab5c0840288f
Stan Konnov <stanislav@jointheview.com>
2022-12-06 13:03:24 +0100

    r squared formual

--- src/services/calculators/r-squared-calculator.service.ts
+++ src/services/calculators/r-squared-calculator.service.ts
@@ -2,6 +2,8 @@
 R-Squared — measures how much the movement of the assets is influenced by the movement of the index.
 Ranges from 0 to 100.
 
+R-Squared = 1 − (Total Variation / Unexplained Variation)
+
 On R-Squared: https://www.investopedia.com/terms/r/r-squared.asp
 */
 
