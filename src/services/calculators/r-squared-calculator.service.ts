c003362ac2f62eefe9d537b6f05b59c01eaf0641
Stan Konnov <stanislav@jointheview.com>
2022-12-06 13:14:50 +0100

    docs

--- src/services/calculators/r-squared-calculator.service.ts
+++ src/services/calculators/r-squared-calculator.service.ts
@@ -5,6 +5,8 @@ Ranges from 0 to 100.
 R-Squared = 1 − (Total Variation / Unexplained Variation)
 
 On R-Squared: https://www.investopedia.com/terms/r/r-squared.asp
+
+On margins: https://groww.in/p/r-squared
 */
 
 export class RSquaredCalculatorService {
