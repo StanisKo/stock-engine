5a1100ff819e1facc032623136485a48781f7c7d
Stan Konnov <stanislav@jointheview.com>
2022-12-06 13:17:23 +0100

    on logic

--- src/services/calculators/r-squared-calculator.service.ts
+++ src/services/calculators/r-squared-calculator.service.ts
@@ -7,6 +7,9 @@ R-Squared = 1 − (Total Variation / Unexplained Variation)
 On R-Squared: https://www.investopedia.com/terms/r/r-squared.asp
 
 On margins: https://groww.in/p/r-squared
+
+The goal is to find investments that will beat the market
+Expect lower r-squared because we're seeking stocks that don’t just match the index
 */
 
 export class RSquaredCalculatorService {
