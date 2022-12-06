7a1585cc1c865afeaf3eaad3a4d8b08f03b1acad
Stan Konnov <stanislav@jointheview.com>
2022-12-06 13:36:57 +0100

    comments

--- src/services/calculators/r-squared-calculator.service.ts
+++ src/services/calculators/r-squared-calculator.service.ts
@@ -4,6 +4,11 @@ Ranges from 0 to 100.
 
 R-Squared = 1 − (Total Variation / Unexplained Variation)
 
+To calculate the total variance, you would subtract the average actual value from each of the actual values,
+square the results and sum them
+From there, divide the first sum of errors (unexplained variance) by the second sum (total variance),
+subtract the result from one, and you have the R-squared
+
 On R-Squared: https://www.investopedia.com/terms/r/r-squared.asp
 
 On margins: https://groww.in/p/r-squared
