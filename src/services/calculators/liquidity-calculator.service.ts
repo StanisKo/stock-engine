e17e8ca88191a005f6aa30736c10c3bc967c6a17
Stan Konnov <stanislav@jointheview.com>
2022-12-09 13:34:15 +0100

    comments

--- src/services/calculators/liquidity-calculator.service.ts
+++ src/services/calculators/liquidity-calculator.service.ts
@@ -1,5 +1,12 @@
 /*
+Current Ratio denotes company’s capacity to meet it’s short-term obligations (debt),
+where short-term obligations are debt due within 1 year period
+
 Current Ratio = Current Assets / Current Liabilities
+
+Quick Ratio similar to Current Ratio, but is more conservative. As a rule, lower than Current Ratio
+
+Quick Ratio = (Cash & Equivalents + Marketable Securities + Account Receivable) / Current Liabilities
 */
 
 export class LiquidityCalculatorService {
