283539427325b5f2d1983fe9daf823e710644cfd
Stan Konnov <stanislav@jointheview.com>
2022-12-12 13:18:04 +0100

    some comments

--- src/services/calculators/debt-calculator.service.ts
+++ src/services/calculators/debt-calculator.service.ts
@@ -1,5 +1,11 @@
 /*
 
+Debt to Equity Measures the relationship between the amount of capital that has been borrowed (debt),
+and the amount of capital contributed by shareholders (equity)
+Displays company’s ability to service it’s long-term debt obligations. The lower the number, the better
+
+Interest Coverage
+
 ****
 
 On Debt to Equity: https://www.investopedia.com/terms/d/debtequityratio.asp
