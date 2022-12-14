f10bd5ab7ef85363a8bed19bba99eb6d1147452b
Stan Konnov <stanislav@jointheview.com>
2022-12-14 13:03:04 +0100

    calculate enterprise multiple

--- src/services/calculators/valuation-calculator.service.ts
+++ src/services/calculators/valuation-calculator.service.ts
@@ -22,12 +22,7 @@ export class ValuationCalculatorService {
 
     static enterpriseValue: number;
 
-    static calculateEnterpriseValue(
-        marketCap: number,
-        debt: number,
-        cash: number,
-        cashAndEquivalents: number,
-    ): void {
+    static calculateEnterpriseValue(marketCap: number, debt: number, cash: number, cashAndEquivalents: number): void {
 
         ValuationCalculatorService.enterpriseValue = marketCap + debt - cash - cashAndEquivalents;
     }
