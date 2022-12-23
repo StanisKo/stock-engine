91553010647c3c6dc5713189150412e2ac60365a
Stan Konnov <stanislav@jointheview.com>
2022-12-23 13:04:54 +0100

    inventory turnover setup

--- src/services/calculators/efficiency-calculator.service.ts
+++ src/services/calculators/efficiency-calculator.service.ts
@@ -1,11 +1,17 @@
 /*
 Asset Turnover = Total Annual Sales / Average Total Assets
 
+Inventory Turnover: COGS / Average Value of Inventory
+
+COGS = Cost of Goods Sold
+
 NOTE: API provides Total Annual Sales as totalRevenue
 
 ****
 
 On Asset Turnover: https://www.investopedia.com/terms/a/assetturnover.asp
+
+On Inventory Turnover: https://www.investopedia.com/terms/i/inventoryturnover.asp
 */
 
 export class EfficiencyCalculatorService {
@@ -13,5 +19,10 @@ export class EfficiencyCalculatorService {
     static calculateAssetTurnover(sales: number, averageTotalAssets: number): number {
 
         return sales / averageTotalAssets;
+    }
+
+    static calculateInventoryTurnover(costOfGoodsSold: number, averageValueOfInventory: number): number {
+
+        return costOfGoodsSold / averageValueOfInventory;
     }
 }
