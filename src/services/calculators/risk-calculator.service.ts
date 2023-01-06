059fcd5158748eb7d0e3121380e5ddb270bcbbbd
Stanislav Konnov <stan@successfactory.com>
2023-01-06 13:16:51 +0100

    grouping risk calculators

--- src/services/calculators/risk-calculator.service.ts
+++ src/services/calculators/risk-calculator.service.ts
@@ -4,6 +4,81 @@ import { ITickerPrice, IBenchmarkPrice, IGenericPrice } from '../../interfaces/t
 
 import { CalculatorHelperService } from '../helpers/calculator-helper.service';
 
+/*
+Standard deviation — indicates how much the current return is deviating from its expected historical normal returns
+The higher standard deviation, the greater possible outcomes, both negative and positive.
+
+SD = SQRT(V)
+
+V = SUM(RoR - ARoR) ^ 2 / N(RoR) - 1
+
+RoR = (P2 - P1) / P1 * 100 where P2 is Nth price and P1 is previous
+
+ARoR = SUM(RoR) / N(RoR) where N is count of datapoints we have
+
+On Standard Deviation: https://www.investopedia.com/terms/s/standarddeviation.asp
+
+****
+
+Sharpe Ratio — measures rate of return on the asset above risk-free investment,
+such as treasury bonds or cash
+
+In other words, measures whether the risk is justified against investing into risk-free assets
+
+Sharpe Ratio = (Ticker RoR - Risk-Free RoR) / Ticker SD
+
+RoR = Rate of Return
+
+SD = Standard Deviation
+
+NOTE: Our Risk-Free Rate of Return is US 1 Year Treasury Bond Yield
+
+On Sharpe Ratio: https://www.investopedia.com/terms/s/sharperatio.asp
+
+****
+
+Alpha — measures excess returns/losses against the return of the index
+
+Alpha = (R – Rf) – Beta * (Rm - Rf)
+
+R = Ticker Rate of Return
+
+Rf = Risk-Free Rate of Return
+
+Beta = Systematic Risk of a Ticker
+
+Rm = Benchmark Rate of Return
+
+On Alpha: https://www.investopedia.com/terms/a/alpha.asp
+
+****
+
+R-Squared — measures how much the movement of the assets is influenced by the movement of the index.
+Ranges from 0 to 100.
+
+NOTE: We're using correlation-based formula to calculate r-squared:
+
+R-Squared = r^2
+
+TR = Ticker Returns
+
+BR = Benchmark Returns
+
+SD = Standard Deviation
+
+r (Correlation) = Covariance(TR, TB) / SD of TR * SD of BR
+
+Covariance = N * SUM(TR * BR) - SUM(TR) * SUM(BR)
+
+N = Dataset Size
+
+On R-Squared: https://www.investopedia.com/terms/r/r-squared.asp
+
+On Correlation: https://www.investopedia.com/terms/c/correlationcoefficient.asp
+
+On Covariance: https://www.investopedia.com/terms/c/covariance.asp
+*/
+
 export class RiskCalculatorService {
 
     /*
@@ -26,7 +101,7 @@ export class RiskCalculatorService {
     }
 
     @Discard
-    static calculateStandardDeviation(prices: ITickerPrice[]): number {
+    public static calculateStandardDeviation(prices: ITickerPrice[]): number {
 
         const [returns, averageRateOfReturn] = CalculatorHelperService.calculateAverageRateOfReturn(
             prices as unknown as IGenericPrice[]
@@ -40,7 +115,7 @@ export class RiskCalculatorService {
     }
 
     @Discard
-    static calculateSharpeRatio(
+    public static calculateSharpeRatio(
         tickerRateOfReturn: number,
         treasuryBondYield: number,
         tickerStandardDeviation: number
@@ -52,7 +127,7 @@ export class RiskCalculatorService {
     }
 
     @Discard
-    static calculateAlpha(
+    public static calculateAlpha(
         tickerRateOfReturn: number,
         benchmarkRateOfReturn: number,
         treasuryBondYield: number,
@@ -68,7 +143,6 @@ export class RiskCalculatorService {
         );
     }
 
-    @Discard
     private static calculateCorrelation(tickerReturns: number[], benchmarkReturns: number[]): number {
 
         const N = tickerReturns.length;
@@ -128,7 +202,7 @@ export class RiskCalculatorService {
     }
 
     @Discard
-    static calculateRSquared(prices: ITickerPrice[], benchmarkPrices: IBenchmarkPrice[]): number {
+    public static calculateRSquared(prices: ITickerPrice[], benchmarkPrices: IBenchmarkPrice[]): number {
 
         const [tickerReturns] = CalculatorHelperService.calculateAverageRateOfReturn(
             prices as unknown as IGenericPrice[]
