cf937ac8432c05ecf84f90aad47bf18f6dfc7529
Stanislav Konnov <stan@successfactory.com>
2023-01-12 22:24:41 +0100

    notes

--- src/services/workers/stock-scoring.worker.ts
+++ src/services/workers/stock-scoring.worker.ts
@@ -6,6 +6,12 @@ import { WeightConfiguratorService } from '../core/weight-configurator.service';
 
 import { mergeSort } from '../../algos/merge-sort.algo';
 
+/*
+TODO: tackle extractor; loop once through all profiles, extract data (check reverts)
+
+loop once again to score: multiple processor classes are required
+*/
+
 
 export default async (industry: string): Promise<IStockProfile[]> => {
 
