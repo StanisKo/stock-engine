a1141734b9fded0d539f52fcb5b2dfe1e3431bd8
Stanislav Konnov <stan@successfactory.com>
2023-01-12 22:04:51 +0100

    Revert "wip on inputs"
    This reverts commit 825c89f7647858bfe1cf1625d8d5deb2f96f4391.

--- src/services/workers/stock-scoring.worker.ts
+++ src/services/workers/stock-scoring.worker.ts
@@ -4,13 +4,7 @@ import { StockProfile } from '../../schemas/stock-profile.schema';
 
 import { WeightConfiguratorService } from '../core/weight-configurator.service';
 
-import { CategoryProcessorService } from '../processors/category-processor.service';
-
 import { mergeSort } from '../../algos/merge-sort.algo';
-
-/*
-TODO: class that gathers input before passing them into processors: on all rations
-*/
 
 
 export default async (industry: string): Promise<IStockProfile[]> => {
@@ -35,9 +29,7 @@ export default async (industry: string): Promise<IStockProfile[]> => {
 
         for (let j = 0; j < categoriesToScore.length; j++) {
 
-            CategoryProcessorService.weightConfiguratorService = weightConfiguratorService;
-
-            CategoryProcessorService.processCategory(categoriesToScore[j], profile, );
+            const category = categoriesToScore[j];
         }
     }
 };
