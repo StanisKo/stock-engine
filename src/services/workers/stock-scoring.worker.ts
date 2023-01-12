775e646d24a235e4ac43ee845a14ec5235d7265e
Stanislav Konnov <stan@successfactory.com>
2023-01-12 13:40:08 +0100

    more setup

--- src/services/workers/stock-scoring.worker.ts
+++ src/services/workers/stock-scoring.worker.ts
@@ -2,10 +2,17 @@ import { IStockProfile } from '../../interfaces/stock-profile.interface';
 
 import { StockProfile } from '../../schemas/stock-profile.schema';
 
+import { WeightConfiguratorService } from '../core/weight-configurator.service';
+
 
 export default async (industry: string): Promise<IStockProfile[]> => {
 
+    const weightConfiguratorService = new WeightConfiguratorService();
+
     const profilesToScore = await StockProfile.find({ industry }).lean();
 
-    
+    for (let i = 0; i < profilesToScore.length; i++) {
+
+
+    }
 }
