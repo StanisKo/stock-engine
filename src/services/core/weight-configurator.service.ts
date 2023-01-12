4f41e62c2281993137672e285b1b69c21af475cf
Stanislav Konnov <stan@successfactory.com>
2023-01-12 12:57:40 +0100

    setup for weight configurator

---
+++ src/services/core/weight-configurator.service.ts
@@ -0,0 +1,30 @@
+/*
+At this point in time, we do not have access to business logic that would
+define weights for each ratio
+
+In such, we deduce weights by simply averaging down each ratio against number
+of ratios on given category
+*/
+
+export class WeightConfiguratorService {
+
+    private static averageWeightOfCategory = 100 / 6;
+
+    private static averageWeightOfRiskRatio = 100 / 5;
+
+    /*
+    We treat CAGR as a separate category
+    */
+    public static cagr = this.averageWeightOfCategory;
+
+
+    public static standardDeviation = this.averageWeightOfRiskRatio;
+
+    public static sharpeRatio = this.averageWeightOfRiskRatio;
+
+    public static beta = this.averageWeightOfRiskRatio;
+
+    public static alpha = this.averageWeightOfRiskRatio;
+
+    public static rSquared = this.averageWeightOfRiskRatio;
+}
