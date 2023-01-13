85ba42a87695f839e5fc1c540b6b68053285c083
Stanislav Konnov <stan@successfactory.com>
2023-01-13 14:13:49 +0100

    establishing db connection from child threads

--- src/services/workers/stock-scoring.worker.ts
+++ src/services/workers/stock-scoring.worker.ts
@@ -1,3 +1,5 @@
+import mongoose from 'mongoose';
+
 import { IStockProfile } from '../../interfaces/stock-profile.interface';
 
 import { StockProfile } from '../../schemas/stock-profile.schema';
@@ -10,6 +12,17 @@ import { RatiosExtractorService } from '../helpers/ratios-extractor.service';
 export default async (industry: string): Promise<IStockProfile[]> => {
 
     const ratiosExtractorService = new RatiosExtractorService();
+
+    /*
+    NOTE: newly spawned threads do not share database connection established in the main thread
+
+    Therefore, we need to manually hook into database from each thread
+    */
+    const { MONGO_PROTOCOL, MONGO_USERNAME, MONGO_PASSWORD, MONGO_HOST, MONGO_PORT, MONGO_DATABASE } = process.env;
+
+    await mongoose.connect(
+        `${MONGO_PROTOCOL}://${MONGO_USERNAME}:${MONGO_PASSWORD}@${MONGO_HOST}:${MONGO_PORT}/${MONGO_DATABASE}`
+    );
 
     const profilesToScore = await StockProfile.find({ industry }).lean();
 
