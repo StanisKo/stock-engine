8a6a1e71b1a89485199eb5031cd0ba64bf6d46a7
Stanislav Konnov <stan@successfactory.com>
2023-01-13 13:26:14 +0100

    add controller

---
+++ src/controllers/stock-scoring.controller.ts
@@ -0,0 +1,12 @@
+import { Request, Response } from 'express';
+
+import { StockScoringService } from '../services/core/stock-scoring.service';
+
+export const profileStocks = async (request: Request, response: Response): Promise<Response> => {
+
+    const stockProfilingService = new StockScoringService();
+
+    const serviceResponse = await stockProfilingService.scoreStocks();
+
+    return response.status(200).json(serviceResponse);
+};
