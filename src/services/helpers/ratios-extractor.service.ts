17b391cb8ccd71392c45ebf24a99f8ffec087179
Stanislav Konnov <stan@successfactory.com>
2023-01-13 11:20:05 +0100

    working through extractor

--- src/services/helpers/ratios-extractor.service.ts
+++ src/services/helpers/ratios-extractor.service.ts
@@ -2,17 +2,41 @@ import { IStockProfile } from '../../interfaces/stock-profile.interface';
 
 export class RatiosExtractorService {
 
-    ratios: { [key: string]: number[] };
+    public ratios: { [key: string]: number[] };
+
+    public categories: string[];
+
+    private keys: string[];
 
     constructor() {
 
         this.ratios = {};
+
+        this.categories = [
+            'risk', 'valuation', 'profitability', 'liquidity', 'debt', 'efficiency'
+        ];
+
+        this.keys = [
+            'cagr', 'standardDeviation', 'sharpeRatio', 'beta', 'alpha', 'rSquared',
+            'priceToEarning', 'priceToEarningsGrowth', 'priceToSales', 'priceToBook',
+            'enterpriseValueToRevenue', 'enterpriseValueToEbitda', 'priceToFreeCashFlow',
+            'returnOnAssets', 'returnOnEquity', 'profitMargin', 'currentRatio', 'quickRatio',
+            'debtToEquity', 'interestCoverage', 'assetTurnover', 'inventoryTurnover'
+        ];
     }
 
     public extractRatiosFromProfiles(profiles: IStockProfile[]): void {
 
         for (let i = 0; i < profiles.length; i++) {
 
+            const profile = profiles[i];
+
+            const keysOfCurrentlyIteratedProfile = Object.keys(profile);
+
+            for (let j = 0; j < keysOfCurrentlyIteratedProfile.length; j++) {
+
+                
+            }
 
         }
     }
